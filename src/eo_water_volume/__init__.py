4e0f5fc174f784a3cf1f869dc1099ab94bbbd984
Fernando E. Romero Galvan <fernando.romerogalvan@gmail.com>
2026-07-07 14:44:58 -0700

    chore: bump version to 0.2.0

--- src/eo_water_volume/__init__.py
+++ src/eo_water_volume/__init__.py
@@ -7,7 +7,7 @@ demand.
 
 from .volume import estimate_volume, volume_map, wse_from_perimeter, summarize
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = [
     "estimate_volume",
     "wse_from_perimeter",
